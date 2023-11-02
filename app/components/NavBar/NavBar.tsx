7d7d0c923ed83635bc5a9c1e7176e2ee116139ed
Toma Bourov <t.bourov@gmail.com>
2023-11-02 09:19:20 +0200

    :zap: Update NavBar

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -1,14 +1,10 @@
 import Link from 'next/link'
-import Image from 'next/image'
 import styles from './NavBar.module.scss'
 
 
 export default function NavBar() {
   return (
     <nav className={styles.nav}>
-      <Link href={'/'}>
-        <Image src='/logo.gif' alt="logo" width={100} height={100} />
-      </Link>
       <ul className={styles.navList}>
         <li className={styles.navItem}>
           <Link href={'/'}>Home</Link>
